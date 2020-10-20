d0c181143203b6a3b4cff74200be7cfcca873b5a
Geraldo Vilela <geraldovilela19@gmail.com>
2020-10-20 10:14:38 -0400

    Initial Commit

---
+++ Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShelvesAndBooks.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titulo de um campo Obrigatorio")]
+        [MaxLength(255, ErrorMessage = "Campo Titulo deve ter no maximo 255 caracteres.")]
+        [MinLength(3, ErrorMessage = "Campo Titulo deve ter no minimo 3 caracateres.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Categoria e um campo obrigatorio")]
+        [MaxLength(60, ErrorMessage = "Campo Categoria deve ter no maximo 60 caracteres.")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Categoria e um campo obrigatorio")]
+        [MaxLength(60, ErrorMessage = "Campo Nome do Autor deve ter no maximo 60 caracteres.")]
+        public string WriterName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Identificador da Prateleira não pode ser invalido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "ID Invalido")]
+        public Shelve ShelveId { get; set; }
+
+
+    }
+}
---
+++ Models/Shelve.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShelvesAndBooks.Models
+{
+    public class Shelve
+    {
+        private int Id { get; set; }
+
+        [Required(ErrorMessage= "Este Campo é Obrigatorio!")]
+        [MaxLength(250, ErrorMessage="Esta Descrição permite até 250 caracteres apenas!")]
+        [MinLength(3, ErrorMessage= "Esta Descrição necessita de pelo menos 3 caracteres!")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Este Campo é Obrigatorio!")]
+        [MaxLength(40, ErrorMessage = "Esta Descrição permite até 40 caracteres apenas!")]
+        [MinLength(3, ErrorMessage = "Esta Descrição necessita de pelo menos 3 caracteres!")]
+        public string Category { get; set; }
+
+    }
+}
